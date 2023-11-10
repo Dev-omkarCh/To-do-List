776f722e0b8e9e76ef2a5f9620232d6d5c16986d
Dev-omkarCh <omkarchikhale2022@gmail.com>
2023-11-10 18:38:15 +0530

    Added a nav

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Starting New project")
+const user = localStorage.getItem("User")||'';
+const nameInput = document.querySelector(".nameInput");
+
+if(localStorage.getItem("User") !== null){
+    nameInput.value = user;
+};
+
+nameInput.addEventListener("blur",()=>{
+    let userName = nameInput.value;
+    localStorage.setItem("User",userName);
+});
