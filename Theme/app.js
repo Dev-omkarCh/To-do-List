62a00e0d74eddcaec213d63b114992e0620b5bf3
Dev-omkarCh <omkarchikhale2022@gmail.com>
2023-11-11 09:09:26 +0530

    Added theme

---
+++ Theme/app.js
@@ -0,0 +1,38 @@
+const theme = document.querySelector(".theme");
+const circle = document.querySelector(".circle");
+const main = document.querySelector(".main");
+let currentTheme = localStorage.getItem("Theme")||"light";
+if(currentTheme === "dark"){  
+   darkTheme();
+}
+else{
+    lightTheme();
+}
+
+theme.addEventListener("click",()=>{
+    if(theme.classList.contains("dark") && currentTheme === "dark"){
+        lightTheme();
+    }
+    else{
+        darkTheme();
+    }
+})
+function lightTheme(){
+    currentTheme = "light";
+    localStorage.setItem("Theme",currentTheme);
+    theme.classList.remove("dark");
+    circle.style.marginLeft = "5px";
+    theme.classList.remove("darkTheme");
+    circle.classList.remove("darkCircle"); 
+    mode.innerText = "Light Theme";
+}
+function darkTheme(){
+    currentTheme = "dark";
+    localStorage.setItem("Theme",currentTheme);
+    theme.classList.add("dark");
+    circle.style.marginLeft = "35px";
+    theme.classList.add("darkTheme");
+    circle.classList.add("darkCircle"); 
+    mode.innerText = "Dark Theme";
+}
+
--- app.js
+++ app.js
@@ -9,3 +9,15 @@ nameInput.addEventListener("blur",()=>{
     let userName = nameInput.value;
     localStorage.setItem("User",userName);
 });
+
+const themeop = document.querySelector("#tri1");
+const themeDiv = document.querySelector(".theme-bar");
+const mode = document.querySelector(".mode");
+themeop.addEventListener("click",()=>{
+    if(themeDiv.classList.contains("show")){
+        themeDiv.classList.remove("show");
+    }
+    else{
+        themeDiv.classList.add("show");        
+    }
+})
